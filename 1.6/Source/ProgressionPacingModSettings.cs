f837aca1641252890ce0a177ea0e23f8d9e3e5b7
Taranchuk <nfhfyxer@gmail.com>
2025-11-27 07:04:33 +0200

    fix

--- 1.6/Source/ProgressionPacingModSettings.cs
+++ 1.6/Source/ProgressionPacingModSettings.cs
@@ -62,6 +62,10 @@ namespace ProgressionPacing
                 {
                     def.baseCost = Mathf.RoundToInt(newCost);
                 }
+                if (def.baseCost < roundingMultiple)
+                {
+                    def.baseCost = roundingMultiple;
+                }
             }
         }
 
